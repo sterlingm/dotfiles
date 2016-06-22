88329e40ab7fb778ca9d08868dace4193a0c0d49
Sterling McLeod <sterlingmcleod9@gmail.com>
2016-06-22 13:26:33 -0400

    Updating ros_conf.sh

--- ros_conf.sh
+++ ros_conf.sh
@@ -11,12 +11,21 @@ source ~/ros_packages/devel/setup.bash
 # Setup the ROS network variables
 export ROS_PORT=11311
 
-#export ROS_IP=192.168.1.102
+export ROS_IP=192.168.1.101
+
+export ROS_MASTER_IP=192.168.1.101
 
 #==========================
 #Local testing
 export ROS_IP=127.0.0.1
+export ROS_MASTER_IP=127.0.0.1
 #==========================
 
-export ROS_MASTER_URI=http://${ROS_IP}:${ROS_PORT}
+export ROS_MASTER_URI=http://${ROS_MASTER_IP}:${ROS_PORT}
 export ROSLAUNCH_SSH_UNKNOWN=1
+
+
+#export TURTLEBOT_BASE=create
+#export TURTLEBOT_STACKS=circle
+#export TURTLEBOT_3D_SENSOR=kinect
+export TURTLEBOT_SERIAL_PORT=/dev/ttyUSB0
